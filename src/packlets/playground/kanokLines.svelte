be0a043a747e865bec95eca1a58a9422ada6974b
Phumrapee Limpianchop <git@rayriffy.com>
2024-08-11 20:52:04 +0700

    chore: staged changes

--- src/packlets/playground/kanokLines.svelte
+++ src/packlets/playground/kanokLines.svelte
@@ -4,15 +4,18 @@
   import { fly } from 'svelte/transition'
   import { flip } from 'svelte/animate'
 
+  export let size: 's' | 'm' | 'l' = 'l'
   export let padding: number = 0
   export let length: number
+
+  const basePadding = size === 'l' ? 8 : size === 'm' ? 4 : 2
 </script>
 
 <div class="flex">
-  <div style={`padding-left: ${8 * padding}rem`}></div>
+  <div style={`padding-left: ${basePadding * padding}rem`}></div>
   {#each Array.from({ length }) as _}
     <Kanok
-      size="l"
+      size={size}
       color={pickRandom(['yellow', 'orange', 'blue', 'green'])}
       variant={pickRandom(['invert-x', 'invert-xy', 'normal'])}
       animate
---
+++ src/packlets/playground/mobileStrip.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import KanokLines from "$playground/kanokLines.svelte";
+</script>
+
+<KanokLines size="m" padding={0} length={12} />
+<KanokLines size="m" padding={0} length={12} />
+<KanokLines size="m" padding={0} length={12} />
+<KanokLines size="m" padding={0} length={12} />
